a85cecd710c8abb8976f937bb053e84a01083b20
NickMark028 <NickMark028@gmail.com>
2021-03-31 20:06:16 +0700

    Add fragment for group

--- Confession/app/src/main/java/com/example/confession/views/HomePageActivity.java
+++ Confession/app/src/main/java/com/example/confession/views/HomePageActivity.java
@@ -2,6 +2,7 @@ package com.example.confession.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
 
 import com.example.confession.R;
