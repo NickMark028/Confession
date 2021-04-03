4fbcf9e3159e34827971e892385bfb48c24a6bc8
thanhvu2901 <vcthanh2901@gmail.com>
2021-04-03 23:27:33 +0700

    navigation

--- Confession/app/src/main/java/com/example/confession/HomePage.java
+++ Confession/app/src/main/java/com/example/confession/HomePage.java
@@ -1,14 +1,87 @@
 package com.example.confession;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomePage extends AppCompatActivity {
+
+	private ActionBar toolbar;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home_page);
-	}
+
+		ImageView v0 = findViewById(R.id.newfeed);
+		v0.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+
+				Fragment selectedFragment = null;
+				selectedFragment = new fragment_newfeed();
+				getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+
+			}
+
+		});
+
+		ImageView v1 = findViewById(R.id.search);
+		v1.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+
+				Fragment selectedFragment = null;
+				selectedFragment = new fragment_search();
+				getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+
+			}
+
+		});
+
+		ImageView v2 = findViewById(R.id.add);
+		v2.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+
+				Fragment selectedFragment = null;
+				selectedFragment = new fragment_add();
+				getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+
+			}
+
+		});
+		ImageView v3 = findViewById(R.id.follow);
+		v3.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+
+				Fragment selectedFragment = null;
+				selectedFragment = new fragment_follow();
+				getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+
+			}
+
+		});
+		ImageView v4 = findViewById(R.id.profie);
+		v4.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+
+				Fragment selectedFragment = null;
+				selectedFragment = new fragment_profie();
+				getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+
+			}
+
+		});
+	};
 }
