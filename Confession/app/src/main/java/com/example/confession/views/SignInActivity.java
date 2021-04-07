e03ae6e2974804e04b405c1cd0ed5adc00e35e53
1753025 <34498550+AnTruongQuoc@users.noreply.github.com>
2021-04-08 04:16:35 +0700

    Update SignInActivity

--- Confession/app/src/main/java/com/example/confession/views/SignInActivity.java
+++ Confession/app/src/main/java/com/example/confession/views/SignInActivity.java
@@ -2,10 +2,12 @@ package com.example.confession.views;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.confession.R;
@@ -16,17 +18,23 @@ public class SignInActivity extends Activity implements SignInBinder.View {
 
 	private SignInBinder.Presenter presenter;
 
-	private EditText txt_username, txt_password;
-	private Button btn_sign_in;
+	private EditText si_username, si_password;
+	private Button si_button, fb_button, google_button;
+	private TextView txt_su_click, forgot_pass_click;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_sign_in);
 
+
+
 		InitPresenter();
 		InitView();
 		InitListener();
+
+//		Intent intent = new Intent(this, SignInActivity.class);
+//		startActivity(intent);
 	}
 
 	private void InitPresenter() {
@@ -35,17 +43,49 @@ public class SignInActivity extends Activity implements SignInBinder.View {
 
 	private void InitView() {
 
-		txt_username = findViewById(R.id.txt_username);
-		txt_password = findViewById(R.id.txt_password);
-		btn_sign_in = findViewById(R.id.btn_login);
+		si_username = findViewById(R.id.si_username);
+		si_password = findViewById(R.id.si_password);
+		si_button = findViewById(R.id.si_button);
+		fb_button = findViewById(R.id.fb_button);
+		google_button = findViewById(R.id.google_button);
+		txt_su_click= findViewById(R.id.txt_su_click);
+		forgot_pass_click= findViewById(R.id.forgot_pass_click);
 	}
 
 	private void InitListener() {
 
-		btn_sign_in.setOnClickListener(new View.OnClickListener() {
+		si_button.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View view) {
-				presenter.HandleLogin(txt_username.getText().toString(), txt_password.getText().toString());
+				presenter.HandleLogin(si_username.getText().toString(), si_password.getText().toString());
+			}
+		});
+
+		fb_button.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				presenter.HandleLogin(si_username.getText().toString(), si_password.getText().toString());
+			}
+		});
+
+		google_button.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				presenter.HandleLogin(si_username.getText().toString(), si_password.getText().toString());
+			}
+		});
+
+		txt_su_click.setOnClickListener(new View.OnClickListener(){
+			@Override
+			public void onClick(View view) {
+				presenter.HandleLogin(si_username.getText().toString(), si_password.getText().toString());
+			}
+		});
+
+		forgot_pass_click.setOnClickListener(new View.OnClickListener(){
+			@Override
+			public void onClick(View view) {
+				presenter.HandleLogin(si_username.getText().toString(), si_password.getText().toString());
 			}
 		});
 	}
