24bb79511d252f59b1555d79b5b1eb7d8193f562
1753025 <34498550+AnTruongQuoc@users.noreply.github.com>
2021-04-14 02:45:42 +0700

    Update CommentAdapter.java

--- Confession/app/src/main/java/com/example/confession/adapters/CommentAdapter.java
+++ Confession/app/src/main/java/com/example/confession/adapters/CommentAdapter.java
@@ -2,13 +2,19 @@ package com.example.confession.adapters;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.drawable.AnimatedVectorDrawable;
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.confession.R;
 import com.example.confession.models.behaviors.ConfessionGroup;
@@ -21,6 +27,13 @@ public class CommentAdapter extends BaseAdapter {
 	Context context;
 	ArrayList<PostComment> comments;
 
+	private LinearLayout ll_cmt_cover;
+	private ImageView iv_ava_user_cmt, iv_cmt_react;
+	private TextView txt_username_message, txt_cmt_time, txt_cmt_likes;
+	private AnimatedVectorDrawable empty_heart, fill_heart;
+	private Drawable drawable;
+	private boolean full = false;
+	private ActionBar actionBar;
 	public CommentAdapter(Context context, ArrayList<PostComment> comments) {
 
 		this.context = context;
@@ -45,16 +58,16 @@ public class CommentAdapter extends BaseAdapter {
 	@Override
 	public View getView(int i, View view, ViewGroup viewGroup) {
 
-//		LayoutInflater inflater = ((Activity)context).getLayoutInflater();
-//		View row = inflater.inflate(R.layout.layout_comment, null);
-//
-//		//  Init view
+		LayoutInflater inflater = ((Activity)context).getLayoutInflater();
+		View row = inflater.inflate(R.layout.layout_comment, null);
+
+		//  Init view
 //		ImageView iv_avatar = row.findViewById(R.id.iv_avatar);
 //		TextView txt_group_name = row.findViewById(R.id.txt_group_name);
 //		TextView txt_member_count = row.findViewById(R.id.txt_member_count);
 //		Button btn_join = row.findViewById(R.id.btn_join);
 //
-//		// Init data
+		// Init data
 //		ConfessionGroup group = groups.get(i);
 ////		iv_avatar.setBackgroundResource(...);
 //		txt_group_name.setText(group.GetGroupInfo().name);
@@ -65,7 +78,50 @@ public class CommentAdapter extends BaseAdapter {
 //			//...
 //		});
 //
-//		return row;
-		return null;
+
+		return row;
+	}
+
+	public void InitView(View view){
+		ll_cmt_cover = view.findViewById(R.id.ll_cmt_cover);
+		iv_ava_user_cmt = view.findViewById(R.id.iv_ava_user_cmt);
+		iv_cmt_react = view.findViewById(R.id.iv_cmt_react);
+		txt_username_message = view.findViewById(R.id.txt_username_message);
+		txt_cmt_time = view.findViewById(R.id.txt_cmt_time);
+		txt_cmt_likes = view.findViewById(R.id.txt_cmt_likes);
+
+		empty_heart = (AnimatedVectorDrawable) context.getResources().getDrawable(R.drawable.avd_heart_empty);
+		fill_heart = (AnimatedVectorDrawable) context.getResources().getDrawable(R.drawable.avd_heart_fill);
+	}
+
+	public void InitListener(){
+		iv_cmt_react.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				HeartAnimate(v);
+			}
+		});
+
+		ll_cmt_cover.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+
+			}
+		});
+
+
+	}
+
+
+
+	private void HeartAnimate(View view) {
+		AnimatedVectorDrawable drawable
+				= full
+				? empty_heart
+				: fill_heart;
+		iv_cmt_react.setImageDrawable(drawable);
+		drawable.start();
+		full = !full;
+//		Toast.makeText(context, full ? "HeartFill" : "HeartEmpty", Toast.LENGTH_SHORT).show();
 	}
 }
