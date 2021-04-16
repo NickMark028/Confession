ee5aa5d88c9de8402edc6a33220ecbaa39002688
1753025 <34498550+AnTruongQuoc@users.noreply.github.com>
2021-04-17 00:50:16 +0700

    Update ProfileUsernameBottomSheet.java
    Change username from default to real name

--- Confession/app/src/main/java/com/example/confession/views/bottomsheet/ProfileUsernameBottomSheet.java
+++ Confession/app/src/main/java/com/example/confession/views/bottomsheet/ProfileUsernameBottomSheet.java
@@ -14,6 +14,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -21,12 +23,15 @@ import androidx.annotation.RequiresApi;
 
 import com.example.confession.R;
 import com.example.confession.binders.BottomSheetListener;
+import com.example.confession.models.behaviors.User;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
 public class ProfileUsernameBottomSheet extends BottomSheetDialogFragment{
     BottomSheetListener mListener;
     private Button profile_logout_btn;
+    private TextView add_post_username;
+    private ImageView add_post_avatar;
     private int result = 0;
 
     @Override
@@ -39,7 +44,14 @@ public class ProfileUsernameBottomSheet extends BottomSheetDialogFragment{
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.bottom_sheet_profile_username,container,false);
 
+
         profile_logout_btn = view.findViewById(R.id.profile_logout_btn);
+        add_post_username = view.findViewById(R.id.add_post_username);
+        add_post_avatar = view.findViewById(R.id.add_post_avatar);
+
+        //SetData
+        add_post_username.setText(User.GetInstance().GetBasicUserInfo().username);
+        //add_post_avatar.setImageResource(User.GetInstance().GetBasicUserInfo().avatar);
 
         profile_logout_btn.setOnClickListener(new View.OnClickListener() {
             @Override
