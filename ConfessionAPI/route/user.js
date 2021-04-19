441328cc2b1ea815d189440e1ddfaae103100f65
18127051 <tphong171@gmail.com>
2021-04-19 20:48:28 +0700

    Build User Router

---
+++ ConfessionAPI/route/user.js
@@ -0,0 +1,12 @@
+const express=require('express');
+const router=express.Router();
+const userController=require('../controller/user');
+router.get('/',userController.index);
+router.post('/register',userController.register);
+
+router.post('/login',userController.login);
+router.get('/profile',userController.getProfile);
+router.post('/profile',userController.updateProfile);
+
+router.get('/joinedconf',userController.getJoinedConfession);
+module.exports=router;
