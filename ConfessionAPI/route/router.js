913eed945e609ef60bcfd99fd4cd2c50ec4cdfad
18127051 <tphong171@gmail.com>
2021-04-19 20:49:32 +0700

    Build Main Router

---
+++ ConfessionAPI/route/router.js
@@ -0,0 +1,20 @@
+const userRouter = require('./user');
+const confessionRouter = require('./confession');
+const postRouter = require('./post');
+class Router
+{
+    route(app)
+    {
+        app.get('/', (req,res)=>{
+            res.send('Confession API');
+        })
+        
+        app.use('/user', userRouter);
+
+        app.use('/confession', confessionRouter);
+
+        app.use('/post', postRouter);
+    }
+}
+
+module.exports=new Router;
