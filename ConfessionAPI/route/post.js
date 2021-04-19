6d6f742be9db951a66721d1453f90a7894638c31
18127051 <tphong171@gmail.com>
2021-04-19 20:49:15 +0700

    Build Post Router

---
+++ ConfessionAPI/route/post.js
@@ -0,0 +1,8 @@
+const express=require('express');
+const router=express.Router();
+const postController=require('../controller/post');
+router.get('/',postController.index);
+router.post('/new',postController.new);
+router.post('/comment/new',postController.newComment);
+router.post('/reaction/react',postController.react);
+module.exports=router;
