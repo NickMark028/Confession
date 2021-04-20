6410bf5e4e77558117017fec1269aed31d03e828
1753025 <34498550+AnTruongQuoc@users.noreply.github.com>
2021-04-21 01:32:41 +0700

    Update YourGroupFragment.java

--- Confession/app/src/main/java/com/example/confession/views/fragments/YourGroupFragment.java
+++ Confession/app/src/main/java/com/example/confession/views/fragments/YourGroupFragment.java
@@ -7,16 +7,25 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.confession.R;
+import com.example.confession.adapters.GroupListAdapter;
+import com.example.confession.adapters.MyOwnGroupAdapter;
+import com.example.confession.binders.JoinedGroupsTabBinder;
+import com.example.confession.models.data.ConfessionGroupInfo;
+import com.example.confession.presenters.JoinedGroupsTabPresenter;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link YourGroupFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class YourGroupFragment extends Fragment {
+public class YourGroupFragment extends Fragment implements JoinedGroupsTabBinder.View{
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,9 +35,13 @@ public class YourGroupFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private int mPage;
     private String mTitle;
+    private Thread newThread;
+    private JoinedGroupsTabBinder.Presenter presenter;
+    private MyOwnGroupAdapter mAdapter;
 
     androidx.appcompat.widget.SearchView your_gr_search;
     ListView lv_your_group_item;
+    LinearLayout ll_your_group_progress;
 
     public YourGroupFragment() {
         // Required empty public constructor
@@ -59,6 +72,8 @@ public class YourGroupFragment extends Fragment {
             mPage = getArguments().getInt(ARG_PARAM1);
             mTitle = getArguments().getString(ARG_PARAM2);
         }
+
+        presenter = new JoinedGroupsTabPresenter(this);
     }
 
     @Override
@@ -67,8 +82,12 @@ public class YourGroupFragment extends Fragment {
         // Inflate the layout for this fragment
         View view =  inflater.inflate(R.layout.fragment_your_group, container, false);
 
-        your_gr_search = view.findViewById(R.id.your_gr_search);;
-        lv_your_group_item = view.findViewById(R.id.lv_your_group_item);;
+        your_gr_search = view.findViewById(R.id.your_gr_search);
+        lv_your_group_item = view.findViewById(R.id.lv_your_group_item);
+        ll_your_group_progress = view.findViewById(R.id.ll_your_group_progress);
+
+        //presenter.HandleGetCreatedGroups();
+
 
         InitListenser();
 
@@ -88,5 +107,57 @@ public class YourGroupFragment extends Fragment {
                 return false;
             }
         });
+    }
+
+    private void UpdateListView() {
+
+        Runnable run = new Runnable() {
+            @Override
+            public void run() {
+                mAdapter.notifyDataSetChanged();
+                lv_your_group_item.invalidateViews();
+                lv_your_group_item.refreshDrawableState();
+            }
+        };
+        run.run();
+    }
+
+    @Override
+    public void OnGetFollowedGroupsSuccess(ArrayList<ConfessionGroupInfo> groups) {
+        //Not in use
+    }
+
+    @Override
+    public void OnGetFollowedGroupsFailure(String error) {
+        Toast.makeText(getContext(), error, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void OnGetCreatedGroupsSuccess(ArrayList<ConfessionGroupInfo> groups) {
+        //Set visibility
+        ll_your_group_progress.setVisibility(View.GONE);
+        lv_your_group_item.setVisibility(View.VISIBLE);
+
+        //set adapter
+        mAdapter = new MyOwnGroupAdapter(getContext(), groups);
+        lv_your_group_item.setAdapter(mAdapter);
+    }
+
+    @Override
+    public void OnGetCreatedGroupsFailure(String error) {
+        Toast.makeText(getContext(), error, Toast.LENGTH_SHORT).show();
+        //Set visibility
+        ll_your_group_progress.setVisibility(View.GONE);
+        lv_your_group_item.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void OnLeaveGroupSuccess(ConfessionGroupInfo group) {
+        //Not in use
+    }
+
+    @Override
+    public void OnLeaveGroupFailure(String error) {
+        //Not in use
     }
 }
