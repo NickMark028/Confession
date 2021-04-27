e98699b32a187e06fcca5da295616d84f6be2019
thanhvu2901 <vcthanh2901@gmail.com>
2021-04-27 16:42:18 +0700

    RemoveCommentPresenter

---
+++ Confession/app/src/main/java/com/example/confession/presenters/postComment_done/RemoveCommentPresenter.java
@@ -0,0 +1,22 @@
+package com.example.confession.presenters.postComment_done;
+
+import com.example.confession.binders.PostComent_done.RemoveCommentBinder;
+
+public class RemoveCommentPresenter implements RemoveCommentBinder.Presenter {
+    private final RemoveCommentBinder.View view;
+
+    public RemoveCommentPresenter(RemoveCommentBinder.View view) {
+        this.view = view;
+    }
+
+    @Override
+    public void HandleRemoveCommentSuccess() {
+        if(true){
+            view.OnRemoveCommentSuccess();
+        }
+        else
+        {
+            view.OnRemoveCommentFail("False");
+        }
+    }
+}
