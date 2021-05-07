037289209f3a5fd624ad0bd3289e5f5b2063a5b8
NickMark028 <NickMark028@gmail.com>
2021-05-08 02:20:29 +0700

    Refactor

--- Confession/app/src/main/java/com/example/confession/presenters/AddCommentPresenter.java
+++ Confession/app/src/main/java/com/example/confession/presenters/AddCommentPresenter.java
@@ -1,27 +1,27 @@
-package com.example.confession.presenters;
-
-import com.example.confession.binders.post.AddCommentBinder;
-
-public class AddCommentPresenter implements AddCommentBinder.Presenter {
-
-	private final AddCommentBinder.View view;
-
-	public AddCommentPresenter(AddCommentBinder.View view) {
-
-		this.view = view;
-	}
-
-//	@Override
-//	public void HandleAddComment(GroupPostInfo post_info, String content) {
+//package com.example.confession.presenters;
 //
-//		BasicUserInfo basic_user_info = User.GetInstance().GetBasicUserInfo();
-//		GroupPost post = new GroupPost(post_info, )
-//		PostCommentInfo comment_info = new PostCommentInfo(post_info, basic_user_info, content);
-//		PostComment comment = post.AddComment(comment_info, basic_user_info);
+//import com.example.confession.binders.post.AddCommentBinder;
 //
-//		if (comment != null)
-//			view.OnAddCommentSuccess();
-//		else
-//			view.OnAddCommentFailure("Failed to add comment");
+//public class AddCommentPresenter implements AddCommentBinder.Presenter {
+//
+//	private final AddCommentBinder.View view;
+//
+//	public AddCommentPresenter(AddCommentBinder.View view) {
+//
+//		this.view = view;
 //	}
-}
+//
+////	@Override
+////	public void HandleAddComment(GroupPostInfo post_info, String content) {
+////
+////		BasicUserInfo basic_user_info = User.GetInstance().GetBasicUserInfo();
+////		GroupPost post = new GroupPost(post_info, )
+////		PostCommentInfo comment_info = new PostCommentInfo(post_info, basic_user_info, content);
+////		PostComment comment = post.AddComment(comment_info, basic_user_info);
+////
+////		if (comment != null)
+////			view.OnAddCommentSuccess();
+////		else
+////			view.OnAddCommentFailure("Failed to add comment");
+////	}
+//}
--- Confession/app/src/main/java/com/example/confession/presenters/post_done/GetCommentsPresenter.java
+++ Confession/app/src/main/java/com/example/confession/presenters/post_done/GetCommentsPresenter.java
@@ -1,7 +1,9 @@
 package com.example.confession.presenters.post_done;
 
+import com.example.confession.binders.PostComent_done.AddCommentBinder;
 import com.example.confession.binders.post.GetCommentsBinder;
 import com.example.confession.models.data.GroupPostInfo;
+import com.example.confession.models.data.PostCommentInfo;
 
 public class GetCommentsPresenter implements GetCommentsBinder.Presenter {
 
@@ -21,4 +23,24 @@ public class GetCommentsPresenter implements GetCommentsBinder.Presenter {
 //		else
 //			view.OnCreateGroupFailure("Duplicate short name");
 	}
+
+	public static class AddCommentPresenter implements AddCommentBinder.Presenter {
+	    private final AddCommentBinder.View view;
+
+	    public AddCommentPresenter(AddCommentBinder.View view) {
+	        this.view = view;
+	    }
+
+
+	    @Override
+	    public void HandleAddComment(PostCommentInfo comment_info, GroupPostInfo post) {
+
+	//        PostComment cmt = new PostComment(comment_info,post);
+	//        PostComment comments = cmt.AddComment(comment_info, User.GetAuthToken());
+	//        if(comments != null){
+	//            view.OnAddCommentSuccess(comments);
+	//        }
+	//        else {
+	//        view.OnAddCommentFailure("Failed to Add Comment!");
+	    }}
 }
