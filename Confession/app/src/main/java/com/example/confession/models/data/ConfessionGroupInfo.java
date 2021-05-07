31da722ed497902b1f686752cc5cb8cb321dc49b
NickMark028 <NickMark028@gmail.com>
2021-05-08 01:16:33 +0700

    Update ConfessionGroupInfo.java

--- Confession/app/src/main/java/com/example/confession/models/data/ConfessionGroupInfo.java
+++ Confession/app/src/main/java/com/example/confession/models/data/ConfessionGroupInfo.java
@@ -1,5 +1,6 @@
 package com.example.confession.models.data;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import java.io.Serializable;
@@ -8,22 +9,28 @@ import java.util.concurrent.CopyOnWriteArrayList;
 public final class ConfessionGroupInfo implements Serializable {
 
 	public final String id;
-	public final String short_name;     // TODO ????
+	public final String short_name;     // Ignore
 	public final String name;
 	public final Object avatar;
 	public int member_count;
 
-	public ConfessionGroupInfo(String short_name, String name) {
+	public ConfessionGroupInfo(String name) {
+		this(name, name);
+	}
 
+	public ConfessionGroupInfo(String short_name, String name) {
 		this(short_name, name, null);
 	}
 
 	public ConfessionGroupInfo(String short_name, String name, Object avatar) {
-
 		this(null, short_name, name, avatar, 0);
 	}
 
-	public ConfessionGroupInfo(String id, String short_name, String name, Object avatar, int member_count) {
+	public ConfessionGroupInfo(String id,
+	                           String short_name,
+	                           String name,
+	                           Object avatar,
+	                           int member_count) {
 
 		this.id = id;
 		this.short_name = short_name;
@@ -32,8 +39,11 @@ public final class ConfessionGroupInfo implements Serializable {
 		this.member_count = member_count;
 	}
 
-	public static ConfessionGroupInfo From(Bundle bundle) {
+	public void AddDataTo(Intent intent) {
+		intent.putExtra("cgi", this);
+	}
 
-		return (ConfessionGroupInfo) bundle.getSerializable("group_info");
+	public static ConfessionGroupInfo From(Intent intent) {
+		return (ConfessionGroupInfo) intent.getSerializableExtra("cgi");
 	}
 }
