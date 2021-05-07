702384f469b37ed38ee0fbb76ee4d7f0999f8c0f
NickMark028 <NickMark028@gmail.com>
2021-05-08 01:04:47 +0700

    Update ConfessionGroupInfo.java

--- Confession/app/src/main/java/com/example/confession/models/data/ConfessionGroupInfo.java
+++ Confession/app/src/main/java/com/example/confession/models/data/ConfessionGroupInfo.java
@@ -10,7 +10,7 @@ public final class ConfessionGroupInfo implements Serializable {
 	public final String id;
 	public final String short_name;     // TODO ????
 	public final String name;
-	public final String avatar;
+	public final Object avatar;
 	public int member_count;
 
 	public ConfessionGroupInfo(String short_name, String name) {
@@ -18,12 +18,12 @@ public final class ConfessionGroupInfo implements Serializable {
 		this(short_name, name, null);
 	}
 
-	public ConfessionGroupInfo(String short_name, String name, String avatar) {
+	public ConfessionGroupInfo(String short_name, String name, Object avatar) {
 
 		this(null, short_name, name, avatar, 0);
 	}
 
-	public ConfessionGroupInfo(String id, String short_name, String name, String avatar, int member_count) {
+	public ConfessionGroupInfo(String id, String short_name, String name, Object avatar, int member_count) {
 
 		this.id = id;
 		this.short_name = short_name;
