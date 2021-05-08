8cf389a8531fc5f1a74e752ece3265ffaf9bd855
1753025 <34498550+AnTruongQuoc@users.noreply.github.com>
2021-05-08 18:59:12 +0700

    Update CreatePostGroupListActivity.java

--- Confession/app/src/main/java/com/example/confession/views/activities/CreatePostGroupListActivity.java
+++ Confession/app/src/main/java/com/example/confession/views/activities/CreatePostGroupListActivity.java
@@ -4,26 +4,32 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.confession.R;
 import com.example.confession.adapters.GroupListAdapter;
+import com.example.confession.binders.CreatePostGroupListBinder;
 import com.example.confession.binders.user.JoinedGroupsBinder;
 import com.example.confession.models.data.ConfessionGroupInfo;
+import com.example.confession.presenters.CreatePostGroupListPresenter;
 import com.example.confession.presenters.user.JoinedGroupsPresenter;
+import com.example.confession.presenters.user.SignInPresenter;
 
 import java.util.ArrayList;
 
-public class CreatePostGroupListActivity extends AppCompatActivity implements JoinedGroupsBinder.View {
+public class CreatePostGroupListActivity extends AppCompatActivity implements CreatePostGroupListBinder.View {
 
+	private CreatePostGroupListBinder.Presenter presenter;
 	private ImageView iv_group_list_back;
 	private ListView lv_create_post_group_list;
-	private JoinedGroupsBinder.Presenter presenter;
     private GroupListAdapter myAdapter;
-
+	private ArrayList<ConfessionGroupInfo> list_groups;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 
@@ -32,20 +38,32 @@ public class CreatePostGroupListActivity extends AppCompatActivity implements Jo
 
 		InitView();
 		InitListener();
+		InitPresenter();
 
-		presenter = new JoinedGroupsPresenter(this);
-		this.runOnUiThread(new Runnable() {
+		//Get joined groups in new thread
+		new Thread(new Runnable() {
 			@Override
 			public void run() {
-				presenter.HandleGetJoinedGroups();
+				presenter.HandleGetGroups();
 			}
-		});
+		}).start();
+
+		Toast.makeText(this, "After call HandleGetGroups", Toast.LENGTH_LONG).show();
+	}
+
+	private void InitPresenter() {
+
+		presenter = new CreatePostGroupListPresenter(this);
 	}
 
 	public void InitView() {
 
 		iv_group_list_back = findViewById(R.id.iv_group_list_back);
 		lv_create_post_group_list = findViewById(R.id.lv_create_post_group_list);
+
+//		list_groups = new ArrayList<>();
+//		myAdapter = new GroupListAdapter(getApplicationContext(), list_groups);
+//		lv_create_post_group_list.setAdapter(myAdapter);
 
 		//presenter.HandleGetGroups();
 	}
@@ -78,14 +96,22 @@ public class CreatePostGroupListActivity extends AppCompatActivity implements Jo
 	}
 
 	@Override
-	public void OnGetJoinedGroupsSuccess(ArrayList<ConfessionGroupInfo> groups) {
-
-		myAdapter = new GroupListAdapter(this, groups);
-		lv_create_post_group_list.setAdapter(myAdapter);
+	public void OnGetGroupsSuccess(ArrayList<ConfessionGroupInfo> groups) {
+		this.runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+				lv_create_post_group_list.setAdapter(new GroupListAdapter(CreatePostGroupListActivity.this, groups));
+			}
+		});
 	}
 
 	@Override
-	public void OnGetJoinedGroupsFailure(String error) {
-
+	public void OnGetGroupsFailure(String error) {
+		this.runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+				Toast.makeText(getApplicationContext(), error, Toast.LENGTH_LONG).show();
+			}
+		});
 	}
 }
