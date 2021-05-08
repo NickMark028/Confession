fa29ada02f9085e17afa016cebe430ec5013e54a
1753025 <34498550+AnTruongQuoc@users.noreply.github.com>
2021-05-08 19:00:32 +0700

    Update NewfeedsFragment.java
    Add Newsfeed call

--- Confession/app/src/main/java/com/example/confession/views/fragments/NewfeedsFragment.java
+++ Confession/app/src/main/java/com/example/confession/views/fragments/NewfeedsFragment.java
@@ -2,17 +2,42 @@ package com.example.confession.views.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.confession.R;
+import com.example.confession.adapters.PostAdapter;
+import com.example.confession.binders.user_done.NewsfeedBinder;
+import com.example.confession.models.behaviors.GroupPost;
 import com.example.confession.models.behaviors.User;
+import com.example.confession.models.data.GroupPostInfo;
+import com.example.confession.presenters.user_done.NewsfeedPresenter;
 
-public class NewfeedsFragment extends Fragment {
+import java.util.ArrayList;
+
+public class NewfeedsFragment extends Fragment implements NewsfeedBinder.View{
+
+	//IN-USE VARIABLES
+	private NewsfeedBinder.Presenter presenter;
+	private SwipeRefreshLayout srl_refresh;
+	private ListView lv_posts;
+	private ArrayList<GroupPostInfo> nf_posts;
+	private PostAdapter postAdapter;
+	private RecyclerView rv_posts;
+
+	public NewfeedsFragment(){
+		presenter = new NewsfeedPresenter(this);
+	}
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -24,10 +49,83 @@ public class NewfeedsFragment extends Fragment {
 	                         Bundle savedInstanceState) {
 		// Inflate the layout for this fragment
 		View view = inflater.inflate(R.layout.fragment_newfeed, container, false);
-
-		ListView lv_posts = view.findViewById(R.id.lv_posts);
-
+		InitView(view);
+		InitListener();
+		CallPresenter();
 
 		return view;
+	}
+
+	@Override
+	public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+		super.onViewCreated(view, savedInstanceState);
+	}
+
+	private void CallPresenter() {
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				presenter.HandleGetNewsfeed();
+			}
+		}).start();
+		srl_refresh.setRefreshing(true);
+		Toast.makeText(getContext(), " Newsfeed on call presenter", Toast.LENGTH_LONG).show();
+	}
+
+	private void InitView(View view){
+		rv_posts = view.findViewById(R.id.rv_posts);
+		srl_refresh = view.findViewById(R.id.srl_refresh);
+		LinearLayoutManager llm = new LinearLayoutManager(getContext());
+
+		llm.setReverseLayout(true);
+		llm.setStackFromEnd(true);
+
+		rv_posts.setLayoutManager(llm);
+
+		nf_posts = new ArrayList<>();
+		postAdapter = new PostAdapter(getContext(), nf_posts);
+		rv_posts.setAdapter(postAdapter);
+	}
+
+	private void InitListener(){
+		srl_refresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+			@Override
+			public void onRefresh() {
+				//Do sth
+				CallPresenter();
+			}
+		});
+	}
+
+
+
+
+	@Override
+	public void OnGetNewsfeedSuccess(ArrayList<GroupPostInfo> posts) {
+		nf_posts.clear();
+		nf_posts.addAll(posts);
+
+		this.getActivity().runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+				srl_refresh.setRefreshing(false);
+
+				postAdapter.notifyDataSetChanged();
+				rv_posts.invalidateItemDecorations();
+				rv_posts.refreshDrawableState();
+			}
+		});
+	}
+
+	@Override
+	public void OnGetNewsfeedFailure(String error) {
+		this.getActivity().runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+				srl_refresh.setRefreshing(false);
+
+				Toast.makeText(getContext(), error, Toast.LENGTH_LONG).show();
+			}
+		});
 	}
 }
