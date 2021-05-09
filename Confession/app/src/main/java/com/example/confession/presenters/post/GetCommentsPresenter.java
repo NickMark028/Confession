f2f3340e9089459574842efb2b3a479d05a6b85a
1753025 <34498550+AnTruongQuoc@users.noreply.github.com>
2021-05-10 02:38:15 +0700

    Update GetCommentsPresenter.java

--- Confession/app/src/main/java/com/example/confession/presenters/post/GetCommentsPresenter.java
+++ Confession/app/src/main/java/com/example/confession/presenters/post/GetCommentsPresenter.java
@@ -3,7 +3,9 @@ package com.example.confession.presenters.post;
 import com.example.confession.binders.post.GetCommentsBinder;
 import com.example.confession.models.behaviors.GroupPost;
 import com.example.confession.models.behaviors.PostComment;
+import com.example.confession.models.behaviors.User;
 import com.example.confession.models.data.GroupPostInfo;
+import com.example.confession.models.data.PostCommentInfo;
 
 import java.util.ArrayList;
 
@@ -19,7 +21,7 @@ public class GetCommentsPresenter implements GetCommentsBinder.Presenter {
 	public void HandleGetComments(GroupPostInfo info) {
 
 		GroupPost post = new GroupPost(info);
-		ArrayList<PostComment> comments = post.GetComment();
+		ArrayList<PostCommentInfo> comments = post.GetComments(User.GetAuthToken());
 
 		if (comments != null)
 			view.OnGetCommentsSuccess(comments);
