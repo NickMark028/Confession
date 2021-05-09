1c50ebbe524fe59fd3f19d60a7a40909d7ce5719
1753025 <34498550+AnTruongQuoc@users.noreply.github.com>
2021-05-10 02:38:47 +0700

    Update _MainActivity.java
    Add LoadingScreen and check token

--- Confession/app/src/main/java/com/example/confession/views/activities/_MainActivity.java
+++ Confession/app/src/main/java/com/example/confession/views/activities/_MainActivity.java
@@ -2,12 +2,17 @@ package com.example.confession.views.activities;
 
 import android.accounts.AccountManager;
 import android.app.Activity;
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
+import android.util.Pair;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.confession.R;
 import com.example.confession.models.behaviors.ConfessionGroup;
@@ -22,20 +27,48 @@ public class _MainActivity extends Activity {
 
 	private SharedPreferences share;
 	private String token;
+	private Thread newThread;
+	private Handler mWait = new Handler();
+	private TextView app_name;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+		app_name = findViewById(R.id.app_name);
+
 		setRequestedOrientation(SCREEN_ORIENTATION_PORTRAIT);
 
 		share = this.getPreferences(MODE_PRIVATE);
 		token = share.getString("token", "");
 
-		Intent intent = new Intent(this, SignInActivity.class);
-		startActivity(intent);
+		if(token.isEmpty()){
+			mWait.postDelayed(new Runnable() {
+				@Override
+				public void run() {
+					try{
+						Intent intent = new Intent(_MainActivity.this, SignInActivity.class);
+						ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(
+								_MainActivity.this,
+								new Pair<View, String>(app_name, "confession"));
+
+						startActivity(intent, options.toBundle());
+					}catch (Exception exc){exc.printStackTrace();}
+				}
+			}, 5000);
+		}
 	}
 
+	@Override
+	public void onBackPressed() {
+		super.onBackPressed();
+	}
 
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+
+		mWait.removeCallbacksAndMessages(null);
+	}
 }
