bf9ba28a0478f0283cb0283069a8efa0039ba892
1753025 <34498550+AnTruongQuoc@users.noreply.github.com>
2021-05-10 02:39:25 +0700

    Update CommentActivity.java
    Load Comment and implement UI

--- Confession/app/src/main/java/com/example/confession/views/activities/CommentActivity.java
+++ Confession/app/src/main/java/com/example/confession/views/activities/CommentActivity.java
@@ -1,73 +1,109 @@
 package com.example.confession.views.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.content.Context;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.RectF;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.confession.R;
 import com.example.confession.adapters.CommentAdapter;
 import com.example.confession.binders.post.AddCommentBinder;
+import com.example.confession.binders.post.GetCommentsBinder;
 import com.example.confession.models.behaviors.PostComment;
 import com.example.confession.models.data.BasicUserInfo;
+import com.example.confession.models.data.ConfessionGroupInfo;
+import com.example.confession.models.data.GroupPostInfo;
 import com.example.confession.models.data.PostCommentInfo;
 import com.example.confession.presenters.post.AddCommentPresenter;
+import com.example.confession.presenters.post.GetCommentsPresenter;
 
 import java.util.ArrayList;
 
-public class CommentActivity extends AppCompatActivity implements AddCommentBinder.View {
+public class CommentActivity extends AppCompatActivity
+		implements GetCommentsBinder.View, AddCommentBinder.View {
 
-    private ArrayList<PostCommentInfo> comments;
+	private AddCommentBinder.Presenter presenterAddCmt;
+	private GetCommentsBinder.Presenter presenterGetCmt;
 
-	private AddCommentBinder.Presenter presenter;
+	private ArrayList<PostCommentInfo> gr_comments;
+	private CommentAdapter cmtAdapter;
+
+	private SwipeRefreshLayout srl_refresh_cmt;
+	private GroupPostInfo gpi;
 	private TextView cmt_txt_post_cmt;
 	private EditText cmt_edit_txt_cmt;
 	private ImageView iv_cmt_back;
-	private ListView lv_cmt_item;
+	private RecyclerView rv_cmt_item;
+	private ItemTouchHelper ith;
+
+	private Thread newThread;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_comment);
 
-		// Todo Testing only
-		SampleData();
+		gpi = (GroupPostInfo)getIntent().getSerializableExtra("gpi");
 
 		InitPresenter();
 		InitView();
 		InitListener();
+
+		LoadComment();
 	}
 
-	private void SampleData()
-    {
-        comments = new ArrayList<>(100);
+	public void LoadComment(){
+		newThread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				presenterGetCmt.HandleGetComments(gpi);
+			}
+		});
 
-//        comments.add(new PostCommentInfo(new BasicUserInfo("user #1" , "Nguyen Minh A1" ), "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede."));
-//        comments.add(new PostCommentInfo(new BasicUserInfo("user #2" , "Nguyen Minh A2" ), "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede."));
-//        comments.add(new PostCommentInfo(new BasicUserInfo("user #3" , "Nguyen Minh A3" ), "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede."));
-//        comments.add(new PostCommentInfo(new BasicUserInfo("user #4" , "Nguyen Minh A4" ), "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede."));
-//        comments.add(new PostCommentInfo(new BasicUserInfo("user #5" , "Nguyen Minh A5" ), "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede."));
-//        comments.add(new PostCommentInfo(new BasicUserInfo("user #6" , "Nguyen Minh A6" ), "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede."));
-//        comments.add(new PostCommentInfo(new BasicUserInfo("user #7" , "Nguyen Minh A7" ), "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede."));
-//        comments.add(new PostCommentInfo(new BasicUserInfo("user #8" , "Nguyen Minh A8" ), "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede."));
-//        comments.add(new PostCommentInfo(new BasicUserInfo("user #9" , "Nguyen Minh A9" ), "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede."));
-//        comments.add(new PostCommentInfo(new BasicUserInfo("user #10", "Nguyen Minh A10"), "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede."));
-//        comments.add(new PostCommentInfo(new BasicUserInfo("user #11", "Nguyen Minh A11"), "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede."));
-//        comments.add(new PostCommentInfo(new BasicUserInfo("user #12", "Nguyen Minh A12"), "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede."));
-//        comments.add(new PostCommentInfo(new BasicUserInfo("user #13", "Nguyen Minh A13"), "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede."));
-//        comments.add(new PostCommentInfo(new BasicUserInfo("user #14", "Nguyen Minh A14"), "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede."));
-//        comments.add(new PostCommentInfo(new BasicUserInfo("user #15", "Nguyen Minh A15"), "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede."));
-   }
+		newThread.start();
+
+		srl_refresh_cmt.setRefreshing(true);
+		//Toast.makeText(CommentActivity.this, "Loading comment...", Toast.LENGTH_LONG).show();
+	}
+
+	public void SendComment(String msg){
+		//Log.e("Check ID------------------","Post Position - " + getLayoutPosition());
+
+		newThread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				presenterAddCmt.HandleAddComment(gpi, msg);
+			}
+		});
+
+		newThread.start();
+	}
 
 	public void InitPresenter() {
 
-		this.presenter = new AddCommentPresenter(this);
+		this.presenterAddCmt = new AddCommentPresenter(this);
+		this.presenterGetCmt = new GetCommentsPresenter(this);
 	}
 
 	public void InitView() {
@@ -75,9 +111,20 @@ public class CommentActivity extends AppCompatActivity implements AddCommentBind
 		cmt_txt_post_cmt = findViewById(R.id.cmt_txt_post_cmt);
 		cmt_edit_txt_cmt = findViewById(R.id.cmt_edit_txt_cmt);
 		iv_cmt_back = findViewById(R.id.iv_cmt_back);
-		lv_cmt_item = findViewById(R.id.lv_cmt_item);
+		srl_refresh_cmt = findViewById(R.id.srl_refresh_cmt);
 
-		lv_cmt_item.setAdapter(new CommentAdapter(this, comments));
+		rv_cmt_item = findViewById(R.id.rv_cmt_item);
+
+		LinearLayoutManager llm = new LinearLayoutManager(getApplicationContext());
+
+		llm.setReverseLayout(true);
+		llm.setStackFromEnd(true);
+
+		rv_cmt_item.setLayoutManager(llm);
+
+		gr_comments = new ArrayList<>();
+		cmtAdapter = new CommentAdapter(CommentActivity.this, gr_comments);
+		rv_cmt_item.setAdapter(cmtAdapter);
 
 		iv_cmt_back.setClickable(false);
 	}
@@ -90,28 +137,46 @@ public class CommentActivity extends AppCompatActivity implements AddCommentBind
 			}
 		});
 
-
-
-		cmt_edit_txt_cmt.addTextChangedListener(new TextWatcher() {
+		srl_refresh_cmt.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
 			@Override
-			public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
+			public void onRefresh() {
+				//Do sth
+				LoadComment();
 			}
+		});
 
+		cmt_edit_txt_cmt.setOnFocusChangeListener(new View.OnFocusChangeListener() {
 			@Override
-			public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-			}
-
-			@Override
-			public void afterTextChanged(Editable s) {
-				if (s.toString().isEmpty()) {
-					iv_cmt_back.setClickable(false);
+			public void onFocusChange(View v, boolean hasFocus) {
+				if (hasFocus) {
+					cmt_txt_post_cmt.setVisibility(View.VISIBLE);
 				} else {
-					iv_cmt_back.setClickable(true);
+					cmt_txt_post_cmt.setVisibility(View.INVISIBLE);
 				}
 			}
 		});
+
+		cmt_edit_txt_cmt.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+			@Override
+			public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+				if (actionId == EditorInfo.IME_ACTION_SEND) {
+					String msg = cmt_edit_txt_cmt.getText().toString();
+//						Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
+
+					SendComment(msg);
+
+					cmt_edit_txt_cmt.setText("");
+					cmt_edit_txt_cmt.clearFocus();
+
+					InputMethodManager imm = (InputMethodManager) v.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+					imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+
+					return true;
+				}
+				return false;
+			}
+		});
+
 
 	}
 
@@ -124,11 +189,58 @@ public class CommentActivity extends AppCompatActivity implements AddCommentBind
 
 	@Override
 	public void OnAddCommentSuccess(PostComment comment) {
-
+		this.runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+				Toast.makeText(CommentActivity.this, "Sent Succesfully", Toast.LENGTH_SHORT).show();
+				LoadComment();
+			}
+		});
 	}
 
 	@Override
 	public void OnAddCommentFailure(String error) {
+		this.runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+				Toast.makeText(CommentActivity.this, "Sent Failed", Toast.LENGTH_SHORT).show();
+			}
+		});
+	}
 
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+
+		if (newThread != null && newThread.isAlive()){newThread.interrupt();}
+	}
+
+	@Override
+	public void OnGetCommentsSuccess(ArrayList<PostCommentInfo> comments) {
+		gr_comments.clear();
+		gr_comments.addAll(comments);
+
+		this.runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+				srl_refresh_cmt.setRefreshing(false);
+
+				cmtAdapter.notifyDataSetChanged();
+				rv_cmt_item.invalidateItemDecorations();
+				rv_cmt_item.refreshDrawableState();
+			}
+		});
+	}
+
+	@Override
+	public void OnGetCommentsFailure(String error) {
+		this.runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+				srl_refresh_cmt.setRefreshing(false);
+
+				Toast.makeText(CommentActivity.this, "Load comment failed", Toast.LENGTH_SHORT).show();
+			}
+		});
 	}
 }
