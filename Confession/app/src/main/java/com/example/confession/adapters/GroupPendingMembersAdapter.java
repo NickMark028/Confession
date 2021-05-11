c9ce6fc02fe5986c03cf02c40c678d9fdabbd531
thanhvu2901 <vcthanh2901@gmail.com>
2021-05-11 10:23:28 +0700

    pendingAdapter

--- Confession/app/src/main/java/com/example/confession/adapters/GroupPendingMembersAdapter.java
+++ Confession/app/src/main/java/com/example/confession/adapters/GroupPendingMembersAdapter.java
@@ -1,10 +1,16 @@
 package com.example.confession.adapters;
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.confession.R;
 
 import java.util.ArrayList;
 
@@ -16,23 +22,33 @@ public class GroupPendingMembersAdapter extends RecyclerView.Adapter<GroupPendin
         this.context = context;
         this.groupPending = groupPending;
     }
+
     @NonNull
     @Override
     public ViewHoler onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(context).inflate(R.layout.layout_pending_members, parent, false);
+        return new GroupPendingMembersAdapter.ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHoler holder, int position) {
+        holder.InitDate(position);
 
     }
-
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
     @Override
     public int getItemCount() {
-        return 0;
+        return groupPending.size() ;
     }
 
+
     public class ViewHoler extends RecyclerView.ViewHolder {
+        private LinearLayout ll_group_pending_members;
+        private ImageView ava_user_pending, accept_pending_member, reject_pending_members;
+        private TextView pending_member_name;
 
         public ViewHoler(@NonNull View itemView) {
             super(itemView);
@@ -40,10 +56,21 @@ public class GroupPendingMembersAdapter extends RecyclerView.Adapter<GroupPendin
             InitListener();
         }
 
-        public void InitListener() {
+        public void InitListener(View view) {
+
         }
 
         public void InitView(View itemView) {
+        }
+
+        public void InitDate(int position) {
+
+        }
+    }
+
+    public class ViewHolder extends ViewHoler {
+        public ViewHolder(View view) {
+            super();
         }
     }
 }
