dcb9712351b6d3eb9004e104d125667dbd7e650d
NickMark028 <NickMark028@gmail.com>
2021-05-11 11:24:15 +0700

    Add UserState enum

---
+++ Confession/app/src/main/java/com/example/confession/models/data/UserState.java
@@ -0,0 +1,18 @@
+package com.example.confession.models.data;
+
+import java.io.Serializable;
+
+public enum UserState implements Serializable {
+
+	NonMember(0),
+	Admin(1),
+	Following(2),
+	Pening(3);
+
+	private int value;
+
+	UserState(int value)
+	{
+		this.value = value;
+	}
+}
