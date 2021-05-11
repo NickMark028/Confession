02d1b49404f9119457848b0399d921aca46a9084
thanhvu2901 <vcthanh2901@gmail.com>
2021-05-11 10:34:36 +0700

    pending members

--- Confession/app/src/main/java/com/example/confession/adapters/GroupPendingMembersAdapter.java
+++ Confession/app/src/main/java/com/example/confession/adapters/GroupPendingMembersAdapter.java
@@ -14,7 +14,7 @@ import com.example.confession.R;
 
 import java.util.ArrayList;
 
-public class GroupPendingMembersAdapter extends RecyclerView.Adapter<GroupPendingMembersAdapter.ViewHoler> {
+public class GroupPendingMembersAdapter extends RecyclerView.Adapter<GroupPendingMembersAdapter.ViewHolder> {
     Context context;
     ArrayList<GroupPendingMembersAdapter> groupPending;
 
@@ -25,13 +25,13 @@ public class GroupPendingMembersAdapter extends RecyclerView.Adapter<GroupPendin
 
     @NonNull
     @Override
-    public ViewHoler onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(context).inflate(R.layout.layout_pending_members, parent, false);
         return new GroupPendingMembersAdapter.ViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHoler holder, int position) {
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         holder.InitDate(position);
 
     }
@@ -45,32 +45,40 @@ public class GroupPendingMembersAdapter extends RecyclerView.Adapter<GroupPendin
     }
 
 
-    public class ViewHoler extends RecyclerView.ViewHolder {
+    public class ViewHolder extends RecyclerView.ViewHolder {
         private LinearLayout ll_group_pending_members;
         private ImageView ava_user_pending, accept_pending_member, reject_pending_members;
         private TextView pending_member_name;
 
-        public ViewHoler(@NonNull View itemView) {
+        public ViewHolder(@NonNull View itemView) {
             super(itemView);
             InitView(itemView);
             InitListener();
+        }
+        public void InitView(View view) {
+            ll_group_pending_members = view.findViewById(R.id.ll_group_pending_members);
+            ava_user_pending = view.findViewById(R.id.ava_user_pending);
+            accept_pending_member = view.findViewById(R.id.accept_pending_member);
+            reject_pending_members = view.findViewById(R.id.reject_pending_members);
+            pending_member_name = view.findViewById(R.id.pending_member_name);
+        }
+
+        public void InitDate(int position) {
+
         }
 
         public void InitListener(View view) {
 
         }
 
-        public void InitView(View itemView) {
-        }
-
-        public void InitDate(int position) {
+        public void InitListener(View view) {
 
         }
+
+
+
+
     }
 
-    public class ViewHolder extends ViewHoler {
-        public ViewHolder(View view) {
-            super();
-        }
-    }
+
 }
