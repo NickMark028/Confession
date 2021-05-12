d6244efce00fdd76fb54b86ef15e2bd93d83f13b
1753025 <34498550+AnTruongQuoc@users.noreply.github.com>
2021-05-13 03:52:16 +0700

    Create BottomSheetCreateNewListener.java

---
+++ Confession/app/src/main/java/com/example/confession/listener/BottomSheetCreateNewListener.java
@@ -0,0 +1,6 @@
+package com.example.confession.listener;
+
+public interface BottomSheetCreateNewListener {
+    void onButtonCreatePostClicked(int result);
+    void onButtonCreateGroupClicked(int result);
+}
