9e2e44bf7ef2dc79a359b076b2ec20c76c282976
NickMark028 <NickMark028@gmail.com>
2021-05-12 17:17:37 +0700

    Update ManagePendingMemberPresenter

--- Confession/app/src/main/java/com/example/confession/binders/group/ManagePendingMembersBinder.java
+++ Confession/app/src/main/java/com/example/confession/binders/group/ManagePendingMembersBinder.java
@@ -1,5 +1,6 @@
 package com.example.confession.binders.group;
 
+import com.example.confession.models.behaviors.ConfessionGroup;
 import com.example.confession.models.data.BasicUserInfo;
 import com.example.confession.models.data.ConfessionGroupInfo;
 
@@ -9,15 +10,16 @@ public interface ManagePendingMembersBinder {
 
 	interface View {
 
-		void OnAcceptAllPendingMembersSuccess();
-
 		void OnAcceptPendingMembersSuccess();
-
 		void OnAcceptPendingMembersFailure(String error);
+
+		void OnRejectPendingMembersSuccess();
+		void OnRejectPendingMembersFailure(String error);
 	}
 
 	interface Presenter {
 
 		void HandleAcceptPendingMembers(BasicUserInfo user_info, ConfessionGroupInfo group_info);
+		void HandleRejectPendingMembers(BasicUserInfo user_info, ConfessionGroupInfo group_info);
 	}
 }
