3d03f258a082c60f2ac73ad846a1143252701cf4
1753025 <34498550+AnTruongQuoc@users.noreply.github.com>
2021-05-13 03:54:01 +0700

    Update JoinedGroupFragment.java

--- Confession/app/src/main/java/com/example/confession/views/fragments/JoinedGroupFragment.java
+++ Confession/app/src/main/java/com/example/confession/views/fragments/JoinedGroupFragment.java
@@ -2,12 +2,14 @@ package com.example.confession.views.fragments;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -37,6 +39,10 @@ public class JoinedGroupFragment extends Fragment implements FollowedGroupsBinde
 	private Thread newThread;
 	private final FollowedGroupsBinder.Presenter presenter;
 	private MyJoinedGroupAdapter mAdapter;
+	private ArrayList<ConfessionGroupInfo> groups_list;
+
+	private ArrayList<ConfessionGroupInfo> origin_item;
+	private ArrayList<ConfessionGroupInfo> search_item;
 
 	androidx.appcompat.widget.SearchView joined_gr_search;
 	ListView lv_joined_group_item;
@@ -84,8 +90,11 @@ public class JoinedGroupFragment extends Fragment implements FollowedGroupsBinde
 		lv_joined_group_item = view.findViewById(R.id.lv_joined_group_item);
 		ll_joined_group_progress = view.findViewById(R.id.ll_joined_group_progress);
 
-		//presenter.HandleGetFollowedGroups();
-		//Toast.makeText(getContext(), " Joined Group onCreateView", Toast.LENGTH_LONG).show();
+		search_item = new ArrayList<>();
+		origin_item = new ArrayList<>();
+		groups_list = new ArrayList<>();
+		mAdapter = new MyJoinedGroupAdapter(getContext(), groups_list);
+		lv_joined_group_item.setAdapter(mAdapter);
 
 		InitListenser();
 		CallPresenter();
@@ -107,29 +116,61 @@ public class JoinedGroupFragment extends Fragment implements FollowedGroupsBinde
 	}
 
 	public void InitListenser() {
+		lv_joined_group_item.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				view.setSelected(true);
+
+				ConfessionGroupInfo cgi = (ConfessionGroupInfo) parent.getItemAtPosition(position);
+				Fragment fragment = GroupFragment.newInstance(cgi);
+
+				((AppCompatActivity)getContext()).getSupportFragmentManager()
+						.beginTransaction()
+						.replace(R.id.fragment_container, fragment, "group_info")
+						.addToBackStack("group_info")
+						.commit();
+			}
+		});
+
 		joined_gr_search.setOnQueryTextListener(new androidx.appcompat.widget.SearchView.OnQueryTextListener() {
 			@Override
 			public boolean onQueryTextSubmit(String query) {
+				groups_list.clear();
+				origin_item.forEach(e -> {
+					if(e.name.contains(query)){
+						groups_list.add(e);
+					}
+				});
+
+				mAdapter.notifyDataSetChanged();
+				lv_joined_group_item.invalidateViews();
+				lv_joined_group_item.refreshDrawableState();
+
 				return false;
 			}
 
 			@Override
 			public boolean onQueryTextChange(String newText) {
-				return false;
+				if(newText.isEmpty()){
+					//search_item.clear();
+					UpdateOriginalListView();
+					return true;
+				}
+				return true;
 			}
 		});
+
+		joined_gr_search.setOnClickListener(v -> joined_gr_search.onActionViewExpanded());
 	}
 
-	private void UpdateListView() {
-		Runnable run = new Runnable() {
-			@Override
-			public void run() {
-				mAdapter.notifyDataSetChanged();
-				lv_joined_group_item.invalidateViews();
-				lv_joined_group_item.refreshDrawableState();
-			}
-		};
-		run.run();
+	private void UpdateOriginalListView() {
+		groups_list.clear();
+		groups_list.addAll(origin_item);
+
+		mAdapter.notifyDataSetChanged();
+		lv_joined_group_item.invalidateViews();
+		lv_joined_group_item.refreshDrawableState();
+
 	}
 
 	@Override
@@ -145,8 +186,10 @@ public class JoinedGroupFragment extends Fragment implements FollowedGroupsBinde
 	public void OnGetFollowedGroupsSuccess(ArrayList<ConfessionGroupInfo> groups) {
 		if(getActivity() == null){ return; }
 
-		Log.e("Successssssssss JG", "Check FJG");
-		//newThread.interrupt();
+		groups_list.clear();
+		groups_list.addAll(groups);
+		origin_item.addAll(groups);
+
 		this.getActivity().runOnUiThread(new Runnable() {
 			@Override
 			public void run() {
@@ -156,19 +199,18 @@ public class JoinedGroupFragment extends Fragment implements FollowedGroupsBinde
 				ll_joined_group_progress.setVisibility(View.GONE);
 				lv_joined_group_item.setVisibility(View.VISIBLE);
 
-				//Set adapter
-				mAdapter = new MyJoinedGroupAdapter(getContext(), groups);
-				lv_joined_group_item.setAdapter(mAdapter);
+				mAdapter.notifyDataSetChanged();
+				lv_joined_group_item.invalidateViews();
+				lv_joined_group_item.refreshDrawableState();
 			}
 		});
 	}
 
 	@Override
 	public void OnGetFollowedGroupsFailure(String error) {
-		//newThread.interrupt();
+
 		if(getActivity() == null){ return; }
 
-		Log.e("Failllllllll JG", "Check FJG");
 		this.getActivity().runOnUiThread(new Runnable() {
 			@Override
 			public void run() {
