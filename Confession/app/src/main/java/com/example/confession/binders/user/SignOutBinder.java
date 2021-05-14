e8c892548cf0c3037740e0ab89533dbe1fee1e08
thanhvu2901 <vcthanh2901@gmail.com>
2021-05-14 22:21:23 +0700

    Update SignOutBinder.java

--- Confession/app/src/main/java/com/example/confession/binders/user/SignOutBinder.java
+++ Confession/app/src/main/java/com/example/confession/binders/user/SignOutBinder.java
@@ -7,7 +7,11 @@ public interface SignOutBinder {
     interface View {
 
         void OnSignOutSuccess();
-
         void OnSignOutFailure(String error);
+    }
+
+    interface Presenter {
+
+        void HandleSignOut(Activity activity);
     }
 }
