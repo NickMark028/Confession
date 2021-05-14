c05d46fe399dd3e49afc28c645c2b1312acb3b79
18127051 <tphong171@gmail.com>
2021-05-14 23:19:03 +0700

    Config add member confession route.

--- ConfessionAPI/route/confession.js
+++ ConfessionAPI/route/confession.js
@@ -6,5 +6,5 @@ router.get('/id',confessionController.getConfessionByID); //
 router.get('/search',confessionController.search); //
 router.post('/new',confessionController.newConfession); //
 router.post('/join',confessionController.joinRequest); //
-router.post('/addmember',confessionController.addMember);
-module.exports=router;
+router.post('/addmember',confessionController.addMember); //
+module.exports=router; 
