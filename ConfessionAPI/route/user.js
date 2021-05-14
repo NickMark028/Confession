6dc16bb6117df6dfd8c131ec4889b081f39acaa3
18127051 <tphong171@gmail.com>
2021-05-14 23:24:48 +0700

    Config update profile route.

--- ConfessionAPI/route/user.js
+++ ConfessionAPI/route/user.js
@@ -6,7 +6,7 @@ router.post('/register',userController.register); //
 
 router.post('/login',userController.login); // 
 router.get('/profile',userController.getProfile); //
-router.post('/profile',userController.updateProfile);
+router.post('/profile',userController.updateProfile); //
 
 router.get('/joinedconf',userController.getJoinedConfession);
 module.exports=router;
