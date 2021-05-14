e31c0801a593f58671e1d57fe014053e85c88faf
18127051 <tphong171@gmail.com>
2021-05-14 23:22:45 +0700

    Config index user route.

--- ConfessionAPI/route/user.js
+++ ConfessionAPI/route/user.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const router=express.Router();
 const userController=require('../controller/user');
-router.get('/',userController.index);
+router.get('/',userController.index); //
 router.post('/register',userController.register);
 
 router.post('/login',userController.login);
