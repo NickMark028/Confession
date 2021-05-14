e7c5aaaac657b491ff452d93a13da63675c38de1
18127051 <tphong171@gmail.com>
2021-05-14 23:21:42 +0700

    Config new comment route.

--- ConfessionAPI/route/post.js
+++ ConfessionAPI/route/post.js
@@ -3,6 +3,6 @@ const router=express.Router();
 const postController=require('../controller/post');
 router.get('/',postController.index); //
 router.post('/new',postController.new); //
-router.post('/comment/new',postController.newComment);
+router.post('/comment/new',postController.newComment); //
 router.post('/reaction/react',postController.react);
 module.exports=router;
