db75a0f5612534df1b5888d045f2e59d334be0a2
18127051 <tphong171@gmail.com>
2021-05-14 23:16:16 +0700

    Config search confession route.

--- ConfessionAPI/route/confession.js
+++ ConfessionAPI/route/confession.js
@@ -3,7 +3,7 @@ const router=express.Router();
 const confessionController=require('../controller/confession'); //
 router.get('/',confessionController.getAllConfession); //
 router.get('/id',confessionController.getConfessionByID); //
-router.get('/search',confessionController.search);
+router.get('/search',confessionController.search); //
 router.post('/new',confessionController.newConfession);
 router.post('/join',confessionController.joinRequest);
 router.post('/addmember',confessionController.addMember);
