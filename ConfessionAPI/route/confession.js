9227b7564c931967bcac0a35177d369e12ceea2f
18127051 <tphong171@gmail.com>
2021-05-14 23:06:36 +0700

    Config confession index route.

--- ConfessionAPI/route/confession.js
+++ ConfessionAPI/route/confession.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const router=express.Router();
 const confessionController=require('../controller/confession'); //
-router.get('/',confessionController.getAllConfession);
+router.get('/',confessionController.getAllConfession); //
 router.get('/id',confessionController.getConfessionByID);
 router.get('/search',confessionController.search);
 router.post('/new',confessionController.newConfession);
