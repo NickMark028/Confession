7df3af87433dba8c7d89913c9025acf78376e7d8
18127051 <tphong171@gmail.com>
2021-05-14 23:34:01 +0700

    Config confession router.

--- ConfessionAPI/route/router.js
+++ ConfessionAPI/route/router.js
@@ -11,7 +11,7 @@ class Router
         
         app.use('/user', userRouter); // 
 
-        app.use('/confession', confessionRouter);
+        app.use('/confession', confessionRouter); //
 
         app.use('/post', postRouter);
     }
