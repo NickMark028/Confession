132c8e25876873471bb1e732ef382fc52d5a00c0
18127051 <tphong171@gmail.com>
2021-05-14 23:21:10 +0700

    Config index route post.

--- ConfessionAPI/route/post.js
+++ ConfessionAPI/route/post.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const router=express.Router();
 const postController=require('../controller/post');
-router.get('/',postController.index);
+router.get('/',postController.index); //
 router.post('/new',postController.new);
 router.post('/comment/new',postController.newComment);
 router.post('/reaction/react',postController.react);
