57ce5e928619d2afd63ea55f0110600cdde4b667
18127051 <tphong171@gmail.com>
2021-05-14 23:16:29 +0700

    Config new confession route.

--- ConfessionAPI/route/confession.js
+++ ConfessionAPI/route/confession.js
@@ -4,7 +4,7 @@ const confessionController=require('../controller/confession'); //
 router.get('/',confessionController.getAllConfession); //
 router.get('/id',confessionController.getConfessionByID); //
 router.get('/search',confessionController.search); //
-router.post('/new',confessionController.newConfession);
+router.post('/new',confessionController.newConfession); //
 router.post('/join',confessionController.joinRequest);
 router.post('/addmember',confessionController.addMember);
 module.exports=router;
