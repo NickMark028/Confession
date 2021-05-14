4368c3294940d0006eba6466b5244fce8668c324
thanhvu2901 <vcthanh2901@gmail.com>
2021-05-14 22:19:47 +0700

    Update JoinGroupBinder.java

--- Confession/app/src/main/java/com/example/confession/binders/user/JoinGroupBinder.java
+++ Confession/app/src/main/java/com/example/confession/binders/user/JoinGroupBinder.java
@@ -2,14 +2,5 @@ package com.example.confession.binders.user;
 
 public interface JoinGroupBinder {
 
-	interface View {
 
-		void OnJoinGroupSuccess();
-		void OnJoinGroupFailure(String error);
-	}
-
-	interface Presenter {
-
-		void HandleJoinGroup(String group_id);
-	}
 }
