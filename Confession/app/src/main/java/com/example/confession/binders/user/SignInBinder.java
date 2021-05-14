164edc2ff005d7e59eae78a6a733cd60f4bdf20c
NickMark028 <NickMark028@gmail.com>
2021-05-14 19:25:52 +0700

    Add reject/accept UI

--- Confession/app/src/main/java/com/example/confession/binders/user/SignInBinder.java
+++ Confession/app/src/main/java/com/example/confession/binders/user/SignInBinder.java
@@ -2,16 +2,34 @@ package com.example.confession.binders.user;
 
 import com.example.confession.models.behaviors.User;
 
+import java.io.Serializable;
+
+//enum SignInErrorMask {
+//
+//
+//
+//	private int value;
+//	public int GetValue() {
+//		return value
+//	}
+//
+//	SignInErrorMask()
+//}
+
 public interface SignInBinder {
+
+//	final int
 
 	interface View {
 
 		void OnLoginSuccess(User user);
+
 		void OnLoginFailure(String error);
 	}
 
 	interface Presenter {
 		void HandleLogin(String username, String password);
+
 		void HandleLogin(String token);
 	}
 }
