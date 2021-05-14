9c5f68337c6d946f0fb56e6be7e81012def9316c
NickMark028 <NickMark028@gmail.com>
2021-05-15 00:21:42 +0700

    Update MembersJoinedGroupActivity.java

--- Confession/app/src/main/java/com/example/confession/views/activities/MembersJoinedGroupActivity.java
+++ Confession/app/src/main/java/com/example/confession/views/activities/MembersJoinedGroupActivity.java
@@ -23,128 +23,130 @@ import com.example.confession.presenters.group.GetMembersPresenter;
 import java.util.ArrayList;
 
 public class MembersJoinedGroupActivity extends AppCompatActivity implements GetMembersBinder.View {
-   private GetMembersBinder.Presenter presenterGetMembers;
+	private GetMembersBinder.Presenter presenterGetMembers;
 
-   private MemberJoinedGroupAdapter membersAdapter;
-   private ArrayList<BasicUserInfo> members_info;
+	private MemberJoinedGroupAdapter membersAdapter;
+	private ArrayList<BasicUserInfo> members_info;
 
-   private SwipeRefreshLayout srl_refresh_members_joined;
-   private ConfessionGroupInfo cgi;
-   private ImageView iv_members_back;
-   private TextView name_member_joined;
-   private ImageView ava_member_joined;
+	private SwipeRefreshLayout srl_refresh_members_joined;
+	private ConfessionGroupInfo cgi;
+	private ImageView iv_members_back;
+	private TextView name_member_joined;
+	private ImageView ava_member_joined;
 
-   private RecyclerView rv_members_joined_item;
-   private ItemTouchHelper ith;
+	private RecyclerView rv_members_joined_item;
+	private ItemTouchHelper ith;
 
-   private Thread newThread;
+	private Thread newThread;
 
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_member_joined_group);
+	@Override
+	protected void onCreate(@Nullable Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_member_joined_group);
 
-        cgi= (ConfessionGroupInfo) getIntent().getSerializableExtra("cgi");
+		cgi = (ConfessionGroupInfo) getIntent().getSerializableExtra("cgi");
 
-        InitPresenter();
-        InitView();
-        InitListener();
+		InitPresenter();
+		InitView();
+		InitListener();
 
-        LoadMembersJoined();
+		LoadMembersJoined();
 
-    }
+	}
 
-    public void LoadMembersJoined() {
-    newThread = new Thread(new Runnable() {
-        @Override
-        public void run() {
-            presenterGetMembers.HandleGetMembers(cgi);
-        }
-    });
-    newThread.start();
-    srl_refresh_members_joined.setRefreshing((true));
-    }
+	public void LoadMembersJoined() {
+		newThread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				presenterGetMembers.HandleGetMembers(cgi);
+			}
+		});
+		newThread.start();
+		srl_refresh_members_joined.setRefreshing((true));
+	}
 
-    public void InitListener() {
-        iv_members_back.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
+	public void InitListener() {
+		iv_members_back.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				finish();
+			}
+		});
 
-        srl_refresh_members_joined.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-                LoadMembersJoined();
-            }
-        });
-    }
+		srl_refresh_members_joined.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+			@Override
+			public void onRefresh() {
+				LoadMembersJoined();
+			}
+		});
+	}
 
-    public void InitView() {
-        srl_refresh_members_joined = findViewById(R.id.srl_refresh_pending);
-        ava_member_joined = findViewById(R.id.ava_member_joined);
-        name_member_joined = findViewById(R.id.name_member_joined);
-        iv_members_back = findViewById(R.id.iv_members_back);
-        srl_refresh_members_joined = findViewById(R.id.srl_refresh_members_joined);
+	public void InitView() {
+		srl_refresh_members_joined = findViewById(R.id.srl_refresh_pending);
+		ava_member_joined = findViewById(R.id.ava_member_joined);
+		name_member_joined = findViewById(R.id.name_member_joined);
+		iv_members_back = findViewById(R.id.iv_members_back);
+		srl_refresh_members_joined = findViewById(R.id.srl_refresh_members_joined);
 
-        rv_members_joined_item = findViewById(R.id.rv_members_joined_item);
+		rv_members_joined_item = findViewById(R.id.rv_members_joined_item);
 
-        LinearLayoutManager llm = new LinearLayoutManager(getApplicationContext());
+		LinearLayoutManager llm = new LinearLayoutManager(getApplicationContext());
 
 //        llm.setReverseLayout(true);
 //        llm.setStackFromEnd(true);
 
-        rv_members_joined_item.setLayoutManager(llm);
+		rv_members_joined_item.setLayoutManager(llm);
 
-        members_info = new ArrayList<>();
-        membersAdapter = new MemberJoinedGroupAdapter(MembersJoinedGroupActivity.this, members_info);
-        rv_members_joined_item.setAdapter(membersAdapter);
+		members_info = new ArrayList<>();
+		membersAdapter = new MemberJoinedGroupAdapter(MembersJoinedGroupActivity.this, members_info);
+		rv_members_joined_item.setAdapter(membersAdapter);
 
-    }
+	}
 
-    public void InitPresenter() {
-        this.presenterGetMembers= new GetMembersPresenter(this);
-    }
+	public void InitPresenter() {
+		this.presenterGetMembers = new GetMembersPresenter(this);
+	}
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        if(newThread!=null && newThread.isAlive()){newThread.interrupt();}
-    }
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		if (newThread != null && newThread.isAlive()) {
+			newThread.interrupt();
+		}
+	}
 
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
-        finish();
-    }
+	@Override
+	public void onBackPressed() {
+		super.onBackPressed();
+		finish();
+	}
 
-    @Override
-    public void OnGetMembersSuccess(ArrayList<BasicUserInfo> members) {
-        members_info.clear();
-        members_info.addAll(members);
+	@Override
+	public void OnGetMembersSuccess(ArrayList<BasicUserInfo> members) {
+		members_info.clear();
+		members_info.addAll(members);
 
-        this.runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                Toast.makeText(MembersJoinedGroupActivity.this, "Get Successfully", Toast.LENGTH_SHORT).show();
+		this.runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+				Toast.makeText(MembersJoinedGroupActivity.this, "Get Successfully", Toast.LENGTH_SHORT).show();
 //                LoadMembersJoined();
-                srl_refresh_members_joined.setRefreshing(false);
+				srl_refresh_members_joined.setRefreshing(false);
 
-                membersAdapter.notifyDataSetChanged();
-                rv_members_joined_item.invalidateItemDecorations();
-                rv_members_joined_item.refreshDrawableState();
-            }
-        });
-    }
+				membersAdapter.notifyDataSetChanged();
+				rv_members_joined_item.invalidateItemDecorations();
+				rv_members_joined_item.refreshDrawableState();
+			}
+		});
+	}
 
-    @Override
-    public void OnGetMembersFailure(String error) {
-        this.runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                Toast.makeText(MembersJoinedGroupActivity.this, "Get Failed", Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
+	@Override
+	public void OnGetMembersFailure(String error) {
+		this.runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+				Toast.makeText(MembersJoinedGroupActivity.this, "Get Failed", Toast.LENGTH_SHORT).show();
+			}
+		});
+	}
 }
