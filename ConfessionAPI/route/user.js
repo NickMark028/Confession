e4f7b1c819fa2e471000e9c9144a1716bd5cfa87
18127051 <tphong171@gmail.com>
2021-05-14 23:23:55 +0700

    Config register account route.

--- ConfessionAPI/route/user.js
+++ ConfessionAPI/route/user.js
@@ -2,7 +2,7 @@ const express=require('express');
 const router=express.Router();
 const userController=require('../controller/user');
 router.get('/',userController.index); //
-router.post('/register',userController.register);
+router.post('/register',userController.register); //
 
 router.post('/login',userController.login);
 router.get('/profile',userController.getProfile);
